0179fcd1c51dd2de5389adca3a6903139aefb23c
kjunith <jimmie.maatta@gmail.com>
2019-11-29 13:27:52 +0100

    29 Nov 2019

---
+++ csharp_grund_lab3/MainPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using System.Text.RegularExpressions;
+
+namespace csharp_grund_lab3
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        private readonly MomsCalculator momsCalc = new MomsCalculator();
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        public void Calculate(object sender, EventArgs args)
+        {
+            momsCalc.Belopp = TrimDoubleValue(InputBelopp.Text);
+            momsCalc.Moms = TrimDoubleValue((sender as Button).Text);
+            OutputMomsSats.Text = TrimDoubleValue((sender as Button).Text).ToString();
+            OutputUtraknatBelopp.Text = string.Format("{0:0.00}", momsCalc.UtraknatBelopp());
+            OutputUtraknadMoms.Text = string.Format("{0:0.00}", momsCalc.UtraknadMoms());
+        }
+
+        private double TrimDoubleValue(string text)
+        {
+            if (text != null)
+            {
+                if (double.TryParse(Regex.Replace(text, "[^.0-9]", ""), out double momsValue))
+                {
+                    return momsValue;
+                }
+            }
+
+            return 0;
+        }
+    }
+}
---
+++ csharp_grund_lab3/MomsCalculator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace csharp_grund_lab3
+{
+    public class MomsCalculator
+    {
+        private double moms;
+
+        public double Belopp { get; set; }
+
+        public double Moms
+        {
+            get { return moms; }
+            set { moms = value / 100; }
+        }
+
+        public double UtraknadMoms()
+        {
+            return Belopp * Moms;
+        }
+
+        public double UtraknatBelopp()
+        {
+            return Belopp - UtraknadMoms();
+        }
+    }
+}
